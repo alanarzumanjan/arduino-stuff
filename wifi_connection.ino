b61c1691d65a71dfc0af269753cc0580a2e2a3ef
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-09-09 15:28:07 +0300

    wifi connection

---
+++ wifi_connection.ino
@@ -0,0 +1,26 @@
+#include <WiFi.h>
+
+const char *ssid = "Network";
+const char *password = "password";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
