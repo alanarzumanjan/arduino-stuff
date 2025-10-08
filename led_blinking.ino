9b412ef1fb0dc09e052b7d8266a347bd4bed5066
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-10-08 11:15:36 +0300

    update

--- led_blinking.ino
+++ led_blinking.ino
@@ -2,21 +2,18 @@ const int leds[] = {15, 16,17, 5, 18, 19, 21, 22, 23};
 const int ledCount = sizeof(leds) / sizeof(leds[0]);
 
 void setup() {
-  // Настраиваем все пины как выходы
   for (int i = 0; i < ledCount; i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
 
 void loop() {
-  // Вперёд
   for (int i = 0; i < ledCount; i++) {
     digitalWrite(leds[i], HIGH);
     delay(100);
     digitalWrite(leds[i], LOW);
   }
 
-  // Назад 
   for (int i = ledCount - 2; i > 0; i--) {
     digitalWrite(leds[i], HIGH);
     delay(100);
